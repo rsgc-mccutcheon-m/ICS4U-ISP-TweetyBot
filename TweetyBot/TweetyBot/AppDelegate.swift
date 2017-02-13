9dec07cd81b9161203cf6abf751c7eebb5e79335
Mark McCutcheon <mark.mccutcheon29@gmail.com>
2017-02-13 09:20:24 -0500

    Hola

---
+++ TweetyBot/TweetyBot/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  TweetyBot
+//
+//  Created by Jarvis on 2017-02-13.
+//  Copyright © 2017 Clutch Design Solutions. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ TweetyBot/TweetyBot/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  TweetyBot
+//
+//  Created by Jarvis on 2017-02-13.
+//  Copyright © 2017 Clutch Design Solutions. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
