0ef62d6ed592ea129d9844b6be6ec34437682680
Mark McCutcheon <mark.mccutcheon29@gmail.com>
2017-02-22 13:58:32 -0500

    Began creating a file reader class for easy file input. this class will be heavily based off of Mr Gordon's Linereader from the mental health week project. (working on issues #2 & #3

---
+++ TweetyBot/TweetyBot/FileReader.swift
@@ -0,0 +1,37 @@
+//
+//  FileReader.swift
+//  TweetyBot
+//
+//  Created by Jarvis on 2017-02-22.
+//  Copyright © 2017 Clutch Design Solutions. All rights reserved.
+//
+
+import Foundation
+
+
+class FileReader {
+    
+    let path: String
+    
+    fileprivate let file: UnsafeMutablePointer<FILE>!
+    
+    init?(path: String) {
+        self.path = path
+        
+        
+        
+        
+        
+        
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
--- TweetyBot/TweetyBot/ViewController.swift
+++ TweetyBot/TweetyBot/ViewController.swift
@@ -14,6 +14,19 @@ class ViewController: NSViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+       
+            
+            }
+            
+        
+        
+        
+        
+        
+        
+        
+    }
     }
 
     override var representedObject: Any? {
