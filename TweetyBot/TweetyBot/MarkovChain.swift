0eb280c90dc51c7cd9b799e906ba15969a670a23
Mark McCutcheon <mmccutcheon@rsgc.on.ca>
2017-03-24 12:39:13 -0400

    Trying to modify my original chain get algorithm to account for two word string suffixes. Not sure if that will work, but if it does it's an easy way of implementing an order 2 chain. #12

--- TweetyBot/TweetyBot/MarkovChain.swift
+++ TweetyBot/TweetyBot/MarkovChain.swift
@@ -59,9 +59,40 @@ class MarkovChain {
         chainGen = true
     }
     
+    
     func gen2suffixChain() {
         
-        for index in words.count-1{
+        for (index, word) in self.words.enumerated() {
+            
+            // Stop loop before no suffix remains
+            if index == words.count - 1 {
+                break
+            }
+            
+            // when the current word from the input array is not in the prefix dictionary at all & skip blank entries
+            if self.prefix[word] == nil && word != " " {
+                
+                self.suffix[(words[index + 1]+words[index+2])] = 1    // add a count of 1 for the suffix (word following current word in input)
+                self.suffix["📐"] = 1                // triangular ruler key represents total # of suffixs count
+                self.prefix[word] = suffix           // current word from input array becomes key in prefix dictionary with value of suffix dictionary just created
+                
+            } else {
+                
+                // add 1 to total in suffix dictionary
+                prefix[word]!["📐"]! += 1
+                
+                // does current word exist as a suffix already?
+                if prefix[word]![words[index + 1]] == nil {         // does not exist, so create and set to 1
+                    prefix[word]![(words[index + 1]+words[index+2])] = 1
+                    
+                } else {                                            // does exist, so increment by 1
+                    prefix[word]![words[index + 1]]! += 1
+                }
+            }
+            
+            // wipe out the suffix dictionary for next iteration, so it starts blank
+            suffix = [:]
+            
             
         }
         
